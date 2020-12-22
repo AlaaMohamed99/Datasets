7a850b0d08266228f9aea29efe7ac09ad1a595e4
AlaaMohamed99 <57914350+AlaaMohamed99@users.noreply.github.com>
2020-12-22 21:41:41 +0200

    Folders of all codes
    You'll find the main code in lib/main.dart

---
+++ form_with_radiobutton/lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Stateful app example",
+    home: Scaffold(appBar: AppBar(title: Text("Simple Form")), body: SIform()),
+  ));
+}
+
+class SIform extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SIformstate();
+  }
+}
+
+enum genderGroup { Male, Female }
+
+class _SIformstate extends State<SIform> {
+  String displaytext = "";
+  genderGroup gendergroup = genderGroup.Male;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      errorStyle:
+                          TextStyle(color: Colors.yellowAccent, fontSize: 15.0),
+                      labelText: 'Enter your name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      errorStyle:
+                          TextStyle(color: Colors.yellowAccent, fontSize: 15.0),
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                      title: Text("Male"),
+                        leading: Radio(
+                      value: genderGroup.Male,
+                      groupValue: gendergroup,
+                      onChanged: (genderGroup gender) {
+                        setState(() {
+                          gendergroup = gender;
+                        });
+                      },
+                    )),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                      title: Text("Female", textScaleFactor: 1,),
+                        leading: Radio(
+                      value: genderGroup.Female,
+                      groupValue: gendergroup,
+                      onChanged: (genderGroup gender) {
+                        setState(() {
+                          gendergroup = gender;
+                        });
+                      },
+                    )),
+                    )
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('Done', textScaleFactor: 1.5),
+                        onPressed: () {
+                          setState(() {
+                            this.displaytext = "You've entered data!";
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+            Text(
+              this.displaytext,
+              textScaleFactor: 1.5,
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ form_without_popup/android/app/src/main/kotlin/com/example/form_without_popup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_without_popup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
