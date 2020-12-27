70887c3e8c7e4aea4f20fe83737e0c2a6ed42a9d
AlaaMohamed99 <57914350+AlaaMohamed99@users.noreply.github.com>
2020-12-28 01:39:05 +0200

    1st app in ionic

---
+++ Flutter/flutter_application_1/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: ButtonClick());
+  }
+}
+
+class ButtonClick extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+        child: Column(
+          children: [
+            Row(
+            children: [RaisedButton(
+            color: Colors.blue,
+            child: Text("Click"),
+            onPressed: () {
+              dialog(context); //action is written here
+            })
+            ])
+            ])
+            );
+  }
+
+  void dialog(BuildContext context) {
+    var alertdialog = AlertDialog(
+      title: Text("local host says"),
+      content: Text("Button Clicked!"),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertdialog;
+        });
+  }
+}
