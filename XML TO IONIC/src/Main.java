6b1d330140d2d3db03822ada706e32a3d4abd894
MostafaMBadawy <36483669+MostafaMBadawy@users.noreply.github.com>
2021-02-18 21:54:43 +0200

    XML TO IONIC

---
+++ XML TO IONIC/src/Main.java
@@ -0,0 +1,20 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        CharStream charStream= CharStreams.fromFileName("./input.xml");
+        XMLLexer lexer = new XMLLexer(charStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        XMLParser parser = new XMLParser(tokens);
+        ParseTree tree = parser.document();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        XML2Ionic XMLListener = new XML2Ionic();
+        walker.walk(XMLListener, tree);
+
+    }
+}
