a25df3202acc56c4a78e109844d9d563f15f95c7
Abdelhameed <56941779+Abdelhameed1998@users.noreply.github.com>
2021-04-15 02:40:32 +0200

    add button_alert and ion-card and ion-badge

---
+++ Ionic/alert/home/home.page.ts
@@ -0,0 +1,21 @@
+import { not } from '@angular/compiler/src/output/output_ast';
+import { Component, ViewChild } from '@angular/core';
+import { IonItemOption, IonItemOptions, IonList } from '@ionic/angular';
+import { Button } from 'protractor';
+import { isNumber } from 'util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+   //<!-- Alert from Button -->
+
+ alert(){
+  window.alert("Button is clicked")
+}
+
+}
---
+++ Ionic/ion-card and ion-badge/home/home.page.ts
@@ -0,0 +1,17 @@
+import { not } from '@angular/compiler/src/output/output_ast';
+import { Component, ViewChild } from '@angular/core';
+import { IonItemOption, IonItemOptions, IonList } from '@ionic/angular';
+import { Button } from 'protractor';
+import { isNumber } from 'util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+ 
+
+}
