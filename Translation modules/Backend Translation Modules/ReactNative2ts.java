5f8adce381182b0ef242b28aaa482f9c887a05da
ahmed taha fekry <35856483+atf01@users.noreply.github.com>
2021-07-17 19:22:12 +0200

    Update ReactNative2ts.java

--- Translation modules/Backend Translation Modules/ReactNative2ts.java
+++ Translation modules/Backend Translation Modules/ReactNative2ts.java
@@ -1,42 +1,29 @@
 import com.sun.org.apache.xpath.internal.Arg;
+import com.sun.tracing.dtrace.FunctionName;
+import jdk.nashorn.internal.ir.FunctionNode;
+
 import javax.security.sasl.SaslServer;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.io.IOException;
 import java.util.Stack;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.antlr.v4.runtime.tree.TerminalNode;
+
 
 public class ReactNative2ts extends JavaScriptParserBaseListener
 {
     private File file;
-    private  boolean inArithmeticop;
+    private ImportStatementHandler importstatementhandler;
     private  boolean in_class;
     private boolean hasSetstate;
     private boolean hasState;
     private  boolean in_class_element;
     private  boolean in_render;
-    private boolean in_paren_exp;
-    private boolean inImport;
     private  boolean inVarDeclaration;
-    private Stack<String> signs;
-    private boolean else_done;
-    private boolean isLeftoperand;
-    private  String TargetFileName;
-    private String TargetClassName;
     private boolean hasElse;
     private boolean inStyles;
     private FileWriter OutputFile;
@@ -50,16 +37,12 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
         Else="";
         hasElse=false;
         inStyles=false;
-        signs = new Stack<String>();
-        inArithmeticop=false;
         inVarDeclaration=false;
         hasSetstate=false;
         hasState=false;
         in_class_element=false;
         in_render=false;
-        inImport=false;
         in_class=false;
-        in_paren_exp=false;
     }
     private void setOutputFile()
     {
@@ -91,15 +74,14 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
     public void onStartTranslation()
     {
-        String startercode = "" +
-                "\nimport { Component} from '@angular/core';\n" +
-                "@Component({\n" +
-                "  selector:"+"'"+"app-home"+"'"+",\n" +
-                "  templateUrl: 'app.html',\n" +
-                "  styleUrls: ['App.scss'],\n" +
-                "})\n";
-        writeInOutputFile(startercode);
-
+        OnStartTranslationHandler handler = new OnStartTranslationHandler();
+        writeInOutputFile(handler.onStartTranslation());
+    }
+	@Override public void enterImportFromBlock(JavaScriptParser.ImportFromBlockContext ctx)
+    {
+        importstatementhandler = new ImportStatementHandler();
+        importstatementhandler.setContext(ctx);
+        writeInOutputFile(importstatementhandler.convertLibrary());
     }
     @Override public void enterExpressionStatement(JavaScriptParser.ExpressionStatementContext ctx)
     {
@@ -112,7 +94,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
         {
             SemiColon+=ctx.eos().SemiColon().getText();
         }
-        if(!(inStyles||in_render))
+        if(!inStyles&&in_render)
         {
             writeInOutputFile(SemiColon+"\n");
         }
@@ -177,7 +159,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
      }
      else
      {
-      if(!(in_render||inStyles))
+      if(!in_render&&!inStyles)
       {
           String parameters="";
       if(ctx.formalParameterList()!=null)
@@ -191,7 +173,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
 	@Override public void exitMethodDefinition(JavaScriptParser.MethodDefinitionContext ctx)
     {
-        boolean combination = !(in_render||inStyles);
+       boolean combination = !in_render&&!inStyles;
        if(combination)
        {
            String OutputCode = ctx.CloseBrace().getText()+"\n";
@@ -200,32 +182,22 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
     @Override public void enterAdditiveExpression(JavaScriptParser.AdditiveExpressionContext ctx)
     {
-          inArithmeticop=true;
-          boolean combination= !(in_render|| inStyles);
-          if (combination)
-          {
-              String sign;
-              if(ctx.Plus()!=null)
-            {
-                sign=ctx.Plus().getText();
-            }
-            else
-            {
-                sign = ctx.Minus().getText();
-            }
-            signs.push(sign);
-          }
 
     }
+    @Override public void enterLiteralExpression(JavaScriptParser.LiteralExpressionContext ctx)
+    {
+        RuleContext Ctx = ctx.parent;
+        ctx.literal().getText();
+    }
+
 
 	@Override public void exitAdditiveExpression(JavaScriptParser.AdditiveExpressionContext ctx)
     {
-        inArithmeticop=false;
+
     }
     	@Override public void enterMultiplicativeExpression(JavaScriptParser.MultiplicativeExpressionContext ctx)
         {
-         inArithmeticop=true;
-         boolean comparison= !(in_render||inStyles);
+         boolean comparison= !in_render&&!inStyles;
          if(comparison)
          {
              String sign;
@@ -241,45 +213,35 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
              {
                  sign = ctx.Modulus().getText();
              }
-             signs.push(sign);
          }
         }
-        @Override public void exitMultiplicativeExpression(JavaScriptParser.MultiplicativeExpressionContext ctx)
-       {
-         inArithmeticop=false;
-       }
 
 	@Override public void enterArgumentsExpression(JavaScriptParser.ArgumentsExpressionContext ctx)
     {
-        String Argument="";
-        boolean Condition = !inVarDeclaration;
-        if(ctx.getChild(0).getText().contains("StyleSheet.create"))
+        ArgumentsHandler argumentshandler = new ArgumentsHandler();
+        argumentshandler.setArguments(ctx);
+        inStyles = argumentshandler.isinStyling();
+        if(!inStyles&&!in_render&&!in_return_statement)
         {
-            inStyles=true;
-        }
-        boolean combination = !(inStyles||in_render||in_return_statement);
-        if(combination)
-        {
-            if (ctx.getChildCount() > 0)
-            {
-                Argument = ctx.getChild(0).getText();
-            }
-            if (Argument.equals("this.setState"))
+            if (ctx.getChild(0).getText().contains("this.setState"))
             {
                 hasSetstate = true;
-                Argument = Argument.replace("setState", "state.");
             }
-            if (Condition)
+            String OutputCode= argumentshandler.convertFunction();
+            if (!inVarDeclaration&&!OutputCode.isEmpty())
             {
-                writeInOutputFile(Argument);
+                writeInOutputFile(OutputCode);
             }
         }
-
     }
     @Override public void exitArgumentsExpression(JavaScriptParser.ArgumentsExpressionContext ctx)
     {
         inStyles=false;
         hasSetstate=false;
+         if(!in_render&&!inStyles)
+        {
+            writeInOutputFile(";\n");
+        }
     }
 
 
@@ -287,24 +249,31 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
 	//start of an argument expressio
 	@Override public void enterArguments(JavaScriptParser.ArgumentsContext ctx)
     {
-        String ArgumentContainer="";
-        boolean combination = !(in_render||inStyles||in_return_statement);
-        if(combination)
+        String FunctionName="";
+        if(ctx.parent.getChild(0)!=null)
+        {
+             FunctionName = ctx.parent.getChild(0).getText();
+        }
+        if(!inStyles&&!in_render&&!in_return_statement)
         {
             //so as not to print an extra ")" for childcount < 3
-            if(ctx.getChildCount()<3)ArgumentContainer="";
+            String ArgumentContainer = "";
+            if(ctx.getChildCount()<3)
+            {
+                ArgumentContainer="";
+            }
             else
             {
                 ArgumentContainer = ctx.getChild(1).getText();
             }
             String OpenParenthes = ctx.OpenParen().getText();
             boolean Condition = !inVarDeclaration;
-            if (hasSetstate)
+            if (FunctionName.equals("this.setState"))
             {
                 ArgumentContainer = ArgumentContainer.replace("{", "").replace("}", "").replace(":", "=");
                 OpenParenthes = "";
             }
-            if (Condition)
+            if(Condition)
             {
               writeInOutputFile(OpenParenthes+ArgumentContainer);
             }
@@ -315,32 +284,15 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     //end of an argument expression
 	@Override public void exitArguments(JavaScriptParser.ArgumentsContext ctx)
     {
-        boolean combination = !(in_render||inStyles);
-        if(combination)
+        String FunctionName = ctx.parent.getChild(0).getText();
+        if(!in_render&&!inStyles)
         {
-        String CloseParenthes = ctx.CloseParen().getText();
-        boolean Condition=hasSetstate||inVarDeclaration||in_return_statement;
-        if(Condition)
+            String CloseParenthes = ctx.CloseParen().getText();
+        if(FunctionName.contains("this.setState")||inVarDeclaration||in_return_statement)
         {
             CloseParenthes="";
         }
           writeInOutputFile(CloseParenthes);
-        }
-    }
-    @Override public void enterMemberDotExpression(JavaScriptParser.MemberDotExpressionContext ctx)
-    {
-        if(!in_render)
-        {
-        boolean comparison=  in_class_element;
-        if(comparison)
-        {
-        //     System.out.print(ctx.getText());
-            if(!signs.isEmpty())
-            {
-       //         System.out.print(signs.pop());
-            }
-        }
-
         }
     }
 
@@ -373,19 +325,18 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
     @Override public void enterArrowFunctionBody(JavaScriptParser.ArrowFunctionBodyContext ctx)
     {
-      boolean combination = !(inStyles||in_render);
+      boolean combination = !inStyles&&!in_render;
       if(combination)
       {
-
-          String openBrace = "{";
-          openBrace+="\n";
-          writeInOutputFile(openBrace);
+          String OpenBrace = "{";
+          OpenBrace+="\n";
+          writeInOutputFile(OpenBrace);
       }
     }
 
 	@Override public void exitArrowFunctionBody(JavaScriptParser.ArrowFunctionBodyContext ctx)
     {
-        boolean conditionCombination = !(in_render||inStyles);
+       boolean conditionCombination = !in_render&&!inStyles;
       if(conditionCombination)
       {
           String OutputCode = "}\n";
@@ -399,16 +350,17 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
 
 	@Override public void exitVariableStatement(JavaScriptParser.VariableStatementContext ctx)
     {
-            System.out.print(inStyles);
             setOutputFile();
-            String semiColon="";
-            boolean condition = !(inStyles);
-            if(ctx.eos().SemiColon()!=null&&condition)
+        if(!inStyles&&!in_render)
             {
-                semiColon=ctx.eos().SemiColon().getText();
-            }
+                String semiColon = "";
+                if(ctx.eos().SemiColon()!=null)
+                {
+                    semiColon=ctx.eos().SemiColon().getText();
+                }
             semiColon+="\n";
             writeInOutputFile(semiColon);
+            }
         inVarDeclaration=false;
     }
 	@Override public void enterVariableDeclarationList(JavaScriptParser.VariableDeclarationListContext ctx)
@@ -433,9 +385,8 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
         boolean condition=!(in_render||inStyles);
         if(condition)
         {
-            String Value = "";
-            Value = ctx.getChild(2).getText();
-                if(inForStatement)
+            String Value = ctx.getChild(2).getText();
+            if(inForStatement)
                 {
                     Value+=";";
                 }
@@ -486,9 +437,11 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
 
 	@Override public void exitBlock(JavaScriptParser.BlockContext ctx)
     {
-        System.out.println(ctx.CloseBrace());
+      if(!in_render&&!inStyles)
+      {
         String OutputCode= ctx.CloseBrace().getText()+"\n";
         writeInOutputFile(OutputCode);
+      }
     }
 
     @Override public void enterAssignmentExpression(JavaScriptParser.AssignmentExpressionContext ctx)
@@ -506,7 +459,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     @Override public void enterReturnStatement(JavaScriptParser.ReturnStatementContext ctx)
     {
         in_return_statement=true;
-        boolean condition = !(in_render||inStyles);
+        boolean condition = !in_render&&!inStyles;
         if(condition)
         {
             String Output = ctx.Return().getText() + " " + ctx.getChild(1).getText();
@@ -516,7 +469,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
 	@Override public void exitReturnStatement(JavaScriptParser.ReturnStatementContext ctx)
     {
         in_return_statement=false;
-        if(!in_render)
+        if(!in_render&&!inStyles)
         {
             String semiColon = ctx.eos().getText();
             writeInOutputFile(semiColon+ "\n");
@@ -531,7 +484,6 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
 
 	@Override public void enterPostIncrementExpression(JavaScriptParser.PostIncrementExpressionContext ctx)
     {
-
            String Output = ctx.getText();
            if(inForStatement)
            {
@@ -541,7 +493,6 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
     @Override public void enterPostDecreaseExpression(JavaScriptParser.PostDecreaseExpressionContext ctx)
     {
-
        String Output = ctx.getText();
        if(inForStatement)
          {
@@ -551,7 +502,7 @@ public class ReactNative2ts extends JavaScriptParserBaseListener
     }
     @Override public void enterRelationalExpression(JavaScriptParser.RelationalExpressionContext ctx)
     {
-        boolean condition = inForStatement&&!(inVarDeclaration||inStyles||in_render||in_return_statement);
+        boolean condition = inForStatement&&!in_render&&!inStyles;
         if(condition)
         {
             String Output= ctx.getText();
